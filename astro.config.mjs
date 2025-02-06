cf38a6105e8b1c2a13f52293658bcc861335fbcf
LeonardTarigan <leontrg32@gmail.com>
2025-02-06 09:53:37 +0700

    feat: opengraph tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import icon from 'astro-icon';
-
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), vue()]
+  site: "https://leonardtarigan.dev",
+  integrations: [tailwind(), icon(), vue(), sitemap()],
 });
